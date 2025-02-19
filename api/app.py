8ba3dae7e5cbb576d38ebc02116ea1d4c665597c
synoraa <recaproy@gmail.com>
2025-02-19 12:32:15 +0530

    update

--- api/app.py
+++ api/app.py
@@ -6,5 +6,10 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+# Vercel needs this for deployment
+def handler(event, context):
+    from flask_lambda import FlaskLambda
+    return FlaskLambda(app)(event, context)
+
 if __name__ == '__main__':
     app.run(debug=True)
